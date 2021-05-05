cd024cc79ca7b9888ff74bb4cadde4376d6968b5
Omoteniola-dev <fatunmbiteniola001@gmail.com>
2021-05-05 19:50:05 +0100

    Created an Aside component on the CreateProfile page

---
+++ src/components/CreateProfile/Aside.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Aside: React.FC<{}> = () => {
+    return(
+        <div className="w-1/3 lg:mt-6 bg-blue-200 rounded-2xl">
+            <p className="p-4 mt-16 text-blue-800 lg:text-3xl text-lg tracking-wider font-bold">Create your profile to be added to the database.</p>
+
+        </div>
+    )
+}
+
+export default Aside;
