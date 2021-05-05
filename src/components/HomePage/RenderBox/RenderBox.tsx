a8a12f31d6fd490161fd95f1e3f99a8c2cc8d611
Omoteniola-dev <fatunmbiteniola001@gmail.com>
2021-05-05 19:43:20 +0100

    Fixed the props

--- src/components/HomePage/RenderBox/RenderBox.tsx
+++ src/components/HomePage/RenderBox/RenderBox.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import EmployeeList from "./EmployeeList";
-
+import { Person } from "../RenderBox/EmployeeList"
 interface EmployeeType {
-    Employees: {},
+    Employees: any,
     setEmployees: any
     Text: string
 }
+
 const RenderBox: React.FC<EmployeeType> = ({Employees, setEmployees, Text}) => {
     fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
             .then(data => setEmployees(data))
     console.log(Employees)
     
+    const filteredList = Employees.filter((Employee: Person) =>{
+        return Employee.name.toLowerCase().includes(Text.toLowerCase())
+    })
     return(
         <div>
-                <EmployeeList Employees={Employees}/>
+                <EmployeeList Employees={filteredList}/>
             
         </div>
     )
