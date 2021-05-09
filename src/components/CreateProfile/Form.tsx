87b44c91fddae04134bea920542db516a29439f8
Teniola Fatunmbi <fatunmbiteniola001@gmail.com>
2021-05-09 05:49:29 -0400

    Handled the inputs of the form

--- src/components/CreateProfile/Form.tsx
+++ src/components/CreateProfile/Form.tsx
@@ -1,46 +1,51 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-const inputValues = ["Firstname", "Lastname", "Email", "Choose username", "Password", "Confirm Password"]
 
 type State = {
-    Firstname: "",
-    Lastname: "",
-    Email: "",
-    Username: "",
-    Password: "",
-    "Confirm Password": "" 
+    profile:{
+        Firstname: "",
+        Lastname: "",
+        Email: "",
+        Username: "",
+        Password: "",
+    }
+    
 }
 
-class Form extends Component {
+class Form extends Component<{}, any> {
     constructor(props: State){
         super(props);
         this.state = {
-            Firstname: "",
-            Lastname: "",
-            Email: "",
-            Username: "",
-            Password: "",
-            "Confirm Password": "" 
-
+            profile:{
+                Firstname: "",
+                Lastname: "",
+                Email: "",
+                Username: "",
+                Password: "",
+            }
+            
         }
     }
+
     
-    handleChange = (e:any) => {
-        this.setState({[e.target.name]: e.currentTarget.value})
-    }
+    
     render(){
-        inputValues.forEach(el => {
-            console.log(this.state[el])
-        });
+        const { profile } = this.state;
+        
+        const handleChange = (e: any) =>{
+        e.preventDefault();
+        profile[e.target.name] = e.currentTarget.value
+        return this.setState({ profile})
+    }
         return (
         <div className="w-full lg:w-1/2 h-full">
             <form className="bg-blue-100 m-4 lg:w-1/2 lg:h-1/2 p-6">
-                {
-                    inputValues.map((placeholder, i)=> {
-                        return <input type="text" name={placeholder} value={this.state[placeholder]} placeholder={placeholder} className="p-3 m-4 border-gray-400 border-b-2 bg-blue-100" onChange={this.handleChange}/>
-                    })
-                }
+                <input type="text" name="Firstname" value={profile.Firstname} placeholder="Firstname" className="p-3 m-4 border-gray-400 border-b-2 bg-blue-100" onChange={handleChange}/>
+                <input type="text" name="Lastname" value={profile.Lastname} placeholder="Lastname" className="p-3 m-4 border-gray-400 border-b-2 bg-blue-100" onChange={handleChange}/>
+                <input type="text" name="Email" value={profile.Email} placeholder="Email" className="p-3 m-4 border-gray-400 border-b-2 bg-blue-100" onChange={handleChange}/>
+                <input type="text" name="Username" value={profile.Username} placeholder="Username" className="p-3 m-4 border-gray-400 border-b-2 bg-blue-100" onChange={handleChange}/>
+                <input type="text" name="Password" value={profile.Password} placeholder="Password" className="p-3 m-4 border-gray-400 border-b-2 bg-blue-100" onChange={handleChange}/>
 
                 <input type="submit" value="Create Profile" className="p-3 bg-blue-500 font-bold m-3 rounded-xl w-4/5 lg:hover:bg-blue-600"/>
                 <small>Already have an account? <Link to="/" className="text-blue-400">Log in</Link></small>
