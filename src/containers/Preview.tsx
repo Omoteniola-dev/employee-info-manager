ec2d3ed3dfb6fd3d348868ec4ab9c7c99207ee89
Teniola <fatunmbiteniola001@gmail.com>
2021-05-10 01:21:19 +0100

    modified the style

--- src/containers/Preview.tsx
+++ src/containers/Preview.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const nav = [
+    {
+        text: "Create profile",
+        link: "./CreateProfile"
+    },
+    {
+        text: "Search",
+        link: "/Homepage"
+    }
+]
 const Preview: React.FC<{}> = () => {
     return(
         <div>
-            <header className="App-header">Welcome to the employee information manager application.
-                <Link to ="./CreateProfile" className="App-link"><p>Create Profile</p></Link>
-                <Link to="/HomePage" className="App-link"><p>Search</p></Link>
+            <header className="App-header font-bold">Welcome to the employee information manager application.
+            <div className="flex flex-wrap lg:flex-row flex-col justify-between mt-6">
+                {
+                    nav.map((n) => {
+                     return <p><Link to={n.link} className="App-link m-5 p-3 bg-gray-500 rounded-xl">{n.text}</Link></p>
+                    })
+                }
+                {/* <p create><Link to ="./CreateProfile" className="App-link">Create Profile</Link></p>
+                <p><Link to="/HomePage" className="App-link">Search</Link></p> */}
+            </div>
+                
             </header>
         </div>
     )
