eb11a718d20ecec11605287905c493c1a96af260
Teniola <fatunmbiteniola001@gmail.com>
2021-05-18 16:36:15 +0100

    Removed the opt prop

--- src/components/HomePage/TopNav/SearchBox.tsx
+++ src/components/HomePage/TopNav/SearchBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { SearchProps } from "./TopNav"
 
-const SearchBox: React.FC<SearchProps> = ({Text, SearchChange, opt, optChange}) => {
+const SearchBox: React.FC<SearchProps> = ({Text, SearchChange}) => {
     return(
         <div className="b-2 text-blue m-4">
             <input type="search" value={Text} placeholder="Search by name" onChange={SearchChange} 
