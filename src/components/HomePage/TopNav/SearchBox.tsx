d997d3680f14af81ea65b9cf90ef765884cc5433
Teniola <fatunmbiteniola001@gmail.com>
2021-05-18 16:33:01 +0100

    Removed the select tag

--- src/components/HomePage/TopNav/SearchBox.tsx
+++ src/components/HomePage/TopNav/SearchBox.tsx
@@ -2,14 +2,8 @@ import React from "react";
 import { SearchProps } from "./TopNav"
 
 const SearchBox: React.FC<SearchProps> = ({Text, SearchChange, opt, optChange}) => {
-    
     return(
         <div className="b-2 text-blue m-4">
-            <select value={opt} onChange={optChange} className="p-2">
-                <option value="Name">Name</option>
-                <option value="Username">Username</option>
-                <option value="Email">Email</option>
-            </select>
             <input type="search" value={Text} placeholder="Search by name" onChange={SearchChange} 
             className="border-2 border-blue p-2"/>
         </div>
