85c7599b743758b17d776f477c403087a874f1f7
Teniola <fatunmbiteniola001@gmail.com>
2021-05-18 16:49:04 +0100

    removed optChange function from HomePage.tsx

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -7,21 +7,15 @@ import RenderBox from "../components/HomePage/RenderBox/RenderBox";
 const HomePage: React.FC<{}> = () => {
     const [Text, setText] = useState<string>("");
     const [Employees, setEmployees] = useState([]);
-    const [opt, setOpt] = useState<string>("");
     
     const SearchChange = (e: React.ChangeEvent<HTMLInputElement>):void =>{ 
         return setText(e.currentTarget.value);
     }
     
-    const optChange= (e: any) => {
-        return setOpt(e.currentTarget.value)
-    }
-
-
     return(
         <div className="h-screen">
-            <TopNav Text={Text} SearchChange={SearchChange} opt={opt} optChange={optChange}/>
-            <RenderBox Employees={Employees} setEmployees={setEmployees} Text={Text} opt={opt}/>
+            <TopNav Text={Text} SearchChange={SearchChange} />
+            <RenderBox Employees={Employees} setEmployees={setEmployees} Text={Text} />
         </div>
     )
 }
