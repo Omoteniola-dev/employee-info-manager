cfd76451bb0e6da749d989c4b2e9e1a5a36432b0
Teniola <fatunmbiteniola001@gmail.com>
2021-05-18 16:08:26 +0100

    modified the layout

--- src/containers/CreateProfile.tsx
+++ src/containers/CreateProfile.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import Form from "../components/CreateProfile/Form"
-import CSS from "csstype"
 const CreateProfile: React.FC<{}> = () => {
-    const style: CSS.Properties = {
-        position: "relative",
-        top: "50%",
-        left: "25%",
-    }
     return(
         <div className="m-2">
             <p className="text-lg lg:text-4xl leading-track font-extrabold text-white ">Create a new user profile</p>
             <div className="lg:relative top-2.5 left-5 ">
                 <Form />
             </div>
-                
                 {/* <Aside /> */}
             
         </div>
