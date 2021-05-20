83c356180d3bc9bde9f53f34b80d32d9476265f4
Teniola <fatunmbiteniola001@gmail.com>
2021-05-20 20:40:30 +0100

    changed the font-size

--- src/containers/CreateProfile.tsx
+++ src/containers/CreateProfile.tsx
@@ -3,7 +3,7 @@ import Form from "../components/CreateProfile/Form"
 const CreateProfile: React.FC<{}> = () => {
     return(
         <div className="m-2">
-            <p className="text-lg lg:text-4xl leading-track font-extrabold text-white ">Create a new user profile</p>
+            <p className="text-lg lg:text-4xl text-3xl leading-track font-extrabold text-white">Create a new user profile</p>
             <div className="lg:relative top-2.5 left-5 ">
                 <Form />
             </div>
